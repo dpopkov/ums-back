08f3f6380c74074fa15f6bc1a21f3b1e55789c30
Denis Popkov <pkvdenis@gmail.com>
2022-03-29 21:09:18 +0300

    Base empty project.

---
+++ src/main/java/learn/ang/umsback/UmsbackApplication.java
@@ -0,0 +1,14 @@
+package learn.ang.umsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmsbackApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/learn/ang/umsback/UmsbackApplicationTests.java
@@ -0,0 +1,14 @@
+package learn.ang.umsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
